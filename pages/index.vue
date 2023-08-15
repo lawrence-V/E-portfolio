5c71b9bfa4468fdbcd88ad41239ecab87cacda9b
lawrence-V <lawrencevaldez027@gmail.com>
2023-08-15 15:40:44 +0800

    update the pages index

--- pages/index.vue
+++ pages/index.vue
@@ -10,8 +10,25 @@
 </template>
 
 <script>
-export default {};
+import Header from "~/components/Header.vue";
+import About from "~/components/About.vue";
+import TechStack from "~/components/TechStack.vue";
+import PersonalProject from "~/components/PersonalProject.vue";
+import ContactMe from "~/components/ContactMe.vue";
+import Footer from "~/components/Footer.vue";
+
+export default {
+  components: {
+    Header,
+    About,
+    TechStack,
+    PersonalProject,
+    ContactMe,
+    Footer,
+  },
+};
 </script>
+
 
 <style>
 </style>
